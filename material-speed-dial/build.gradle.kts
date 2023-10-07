9c9ec3facdcecca9b01d9edfa02659dd1907e3d3
opoojkk <gotcha1874@gmail.com>
2023-10-07 08:43:36 +0800

    refactor build.gradle format in library module

--- material-speed-dial/build.gradle.kts
+++ material-speed-dial/build.gradle.kts
@@ -16,46 +16,45 @@ publishing {
             }
         }
     }
+}
+android {
+    namespace = "com.joykeepsflowin.material_speed_dial"
+    compileSdk = 33
 
-    android {
-        namespace = "com.joykeepsflowin.material_speed_dial"
-        compileSdk = 33
+    defaultConfig {
+        minSdk = 24
 
-        defaultConfig {
-            minSdk = 24
-
-            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-            consumerProguardFiles("consumer-rules.pro")
-        }
-
-        buildTypes {
-            release {
-                isMinifyEnabled = false
-                proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
-                )
-            }
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-        }
-        kotlinOptions {
-            jvmTarget = "17"
-        }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
-    dependencies {
-
-        implementation("androidx.core:core-ktx:1.9.0")
-        implementation("androidx.appcompat:appcompat:1.6.1")
-        implementation("com.google.android.material:material:1.9.0")
-        testImplementation("junit:junit:4.13.2")
-        androidTestImplementation("androidx.test.ext:junit:1.1.5")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-        // speed-dial
-        api("com.leinardi.android:speed-dial:3.3.0")
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10")
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // speed-dial
+    api("com.leinardi.android:speed-dial:3.3.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10")
 }
